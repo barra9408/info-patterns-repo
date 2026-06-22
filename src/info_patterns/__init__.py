a44e75da7e371a83ff214494aec65b3a3606c1fe
barra9408 <barra.sgb94@gmail.com>
2026-06-21 21:01:33 -0600

    Modified __init_.py

---
+++ src/info_patterns/__init__.py
@@ -0,0 +1,28 @@
+"""
+info_patterns
+
+Library for optical information-pattern simulations of nanoparticles.
+"""
+
+# Import full submodules
+from . import constants
+from . import generate_nanoparticle
+from . import information_patterns_simulation
+from . import light_matter_interaction_simulation
+from . import measurement_tools
+from . import parameters
+from . import plots
+from . import specific_incident_fields
+
+# Import all public objects from each submodule into the package namespace
+from .constants import *
+from .generate_nanoparticle import *
+from .information_patterns_simulation import *
+from .light_matter_interaction_simulation import *
+from .measurement_tools import *
+from .parameters import *
+from .plots import *
+from .specific_incident_fields import *
+
+# Build __all__ automatically from all imported public names
+__all__ = [name for name in globals() if not name.startswith("_")]
