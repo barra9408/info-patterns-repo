df36dd5ce27b820f09bef243b1eb068325d4f32c
barra9408 <barra.sgb94@gmail.com>
2026-07-24 14:21:22 -0600

    Modified src code

--- src/info_patterns/measurement_tools.py
+++ src/info_patterns/measurement_tools.py
@@ -541,7 +541,7 @@ def heating_rate(S_FF: float, mass_kg: float, Omega_rad_s: float) -> float:
     """
     Compute the recoil heating rate Gamma_mu.
 
-    Gamma_mu = S_FF / (2 * m * hbar * Omega_mu)
+    Gamma_mu = pi * S_FF / (m * hbar * Omega_mu)
 
     Parameters
     ----------
@@ -560,6 +560,6 @@ def heating_rate(S_FF: float, mass_kg: float, Omega_rad_s: float) -> float:
         Recoil heating rate in phonons/s.
     """
 
-    Gamma_mu = S_FF / (2.0 * mass_kg * HBAR * Omega_rad_s)
+    Gamma_mu = np.pi * S_FF / (mass_kg * HBAR * Omega_rad_s)
 
     return float(Gamma_mu)
